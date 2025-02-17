5ce5b30c4ba5795b70e99a031eac72273acde049
James Byrne <jambyr@bas.ac.uk>
2025-02-17 23:16:09 +0000

    HEMI added to logs

--- scripts/prep_cmip_training_data.sh
+++ scripts/prep_cmip_training_data.sh
@@ -23,9 +23,9 @@ if [ $DOWNLOAD -eq 1 ]; then
       echo "MEMBER: $MEMBER"
       COMMAND="download_cmip $DATA_ARGS --source $SOURCE --member $MEMBER $HEMI $CMIP6_DATES $CMIP6_VAR_ARGS"
       echo -e "\n\n$COMMAND\n\n"
-      $COMMAND >logs/download.cmip_training.${SOURCE}.${MEMBER}.log 2>&1
+      $COMMAND >logs/download.cmip_${HEMI}.${SOURCE}.${MEMBER}.log 2>&1
     done
-  done 2>&1 | tee logs/download.cmip_training.log
+  done 2>&1 | tee logs/download.cmip_${HEMI}.log
 fi
 
   # download_cmip --source MRI-ESM2-0 --member r1i1p1f1 $DATA_ARGS $HEMI $CMIP6_DATES $CMIP6_VAR_ARGS
